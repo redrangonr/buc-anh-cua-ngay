6c062ef9d98ba52913e6d46f6c82936cd56107d8
Quocanh <phiquocanh717@gmail.com>
2021-07-14 16:27:29 +0700

    sasa

---
+++ bai 1/src/main/java/com/codegym/cotroller/GretingController.java
@@ -0,0 +1,18 @@
+package com.codegym.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GretingController {
+
+    @GetMapping("/greeting")
+    public String greting(Model model){
+        model.addAttribute("name","asda");
+        return "/index";
+    }
+
+
+}
---
+++ bai 1/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cyber
+  Date: 7/14/2021
+  Time: 8:47 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+ hello world : ${name}
+
+  </body>
+</html>
